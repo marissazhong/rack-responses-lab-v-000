9338296f98e8233e8b8b03362fad93869c42f74e
Marissa Zhong <mwz2101@columbia.edu>
2018-07-10 16:39:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-
+    if Time.now > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
     resp.finish
   end
 end
